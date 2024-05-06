8517490453dbbb109fc866fc638a10906e392f29
Nisharma18 <139926352+Nisharma18@users.noreply.github.com>
2024-05-06 19:29:17 +0000

    switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,62 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 3
+
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("feb");
+        break;
+    case 3:
+        console.log("march");
+        break;
+    case 4:
+        console.log("april");
+        break;
+
+    case 5:
+        console.log("May");
+        break;
+
+    case 6:
+        console.log("june");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
+
+
+
+
+
+const months = "march"
+
+switch (months) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
